46ba742f5caffe367fff1915ffac1e1fd82ff230
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:29:00 -0300

    docs: document engine methods

--- src/CardGameEngine/Action/Implementations/Deck/StackedDeck/RemoveCardFromStackedDeckAction.cs
+++ src/CardGameEngine/Action/Implementations/Deck/StackedDeck/RemoveCardFromStackedDeckAction.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
 
 namespace CardGameEngine;
@@ -13,6 +13,10 @@ public class RemoveCardFromStackedDeckAction : Action
     public readonly IStackedDeck deck;
 
     [JsonConstructor]
+/// <summary>Initializes a new instance of the <see cref="RemoveCardFromStackedDeckAction"/> type.</summary>
+/// <param name="deck">The deck value.</param>
+/// <param name="card">The card value.</param>
+/// <param name="isAborted">The isAborted value.</param>
     public RemoveCardFromStackedDeckAction(IStackedDeck deck, ICard card, bool isAborted = false)
     {
         this.deck = deck;
@@ -20,16 +24,23 @@ public class RemoveCardFromStackedDeckAction : Action
         IsAborted = isAborted;
     }
 
+/// <summary>Executes this operation against the specified game.</summary>
+/// <param name="game">The game value.</param>
     public override void Execute(IGame game)
     {
         Card = deck.Pop();
     }
 
+/// <summary>Determines whether this operation can be executed for the specified game state.</summary>
+/// <param name="gameState">The gameState value.</param>
+/// <returns>The result of the operation.</returns>
     public override bool IsExecutable(IGameState gameState)
     {
         return !deck.IsEmpty;
     }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
     public override object Clone()
     {
         return new RemoveCardFromStackedDeckAction(
