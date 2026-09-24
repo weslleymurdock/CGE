ea410c7a328b924585bb6ed365cb9dc1f2803c56
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:31:01 -0300

    docs: document engine methods

--- src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/EndPlayCardEvent.cs
+++ src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/EndPlayCardEvent.cs
@@ -1,18 +1,24 @@
-﻿using System;
+using System;
 
 namespace CardGameEngine
 {
     [Serializable]
     public class EndPlayCardEvent : CardEvent
     {
+/// <summary>Initializes a new instance of the <see cref="EndPlayCardEvent"/> type.</summary>
+/// <param name="base(card">The base(card value.</param>
         public EndPlayCardEvent(ICard card) : base(card)
         {
         }
 
+/// <summary>Initializes a new instance of the <see cref="EndPlayCardEvent"/> type.</summary>
+/// <param name="base(getCard">The base(getCard value.</param>
         public EndPlayCardEvent(Func<ICard> getCard) : base(getCard)
         {
         }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
         public override EndPlayCardEvent Clone()
         {
             return new EndPlayCardEvent((ICard)Card.Clone());
