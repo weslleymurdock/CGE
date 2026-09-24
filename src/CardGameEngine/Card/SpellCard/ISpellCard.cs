9e7d891be9314b945e41d141de5ab18d27066262
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:33:20 -0300

    docs: complete card interface method documentation

--- src/CardGameEngine/Card/SpellCard/ISpellCard.cs
+++ src/CardGameEngine/Card/SpellCard/ISpellCard.cs
@@ -2,5 +2,6 @@
 
 public interface ISpellCard : ICard
 {
+    /// <summary>Determines whether the card can currently be cast for the specified game state.</summary>
     bool IsCastable(IGameState gameState);
 }
