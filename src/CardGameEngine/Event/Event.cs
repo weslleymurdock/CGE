97c2f7d1d420bc12cda1456b3638890dfa7545bb
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:30:34 -0300

    docs: document engine methods

--- src/CardGameEngine/Event/Event.cs
+++ src/CardGameEngine/Event/Event.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
 
 namespace CardGameEngine
@@ -7,15 +7,21 @@ namespace CardGameEngine
     public abstract class Event : Action
     {
         [JsonConstructor]
+/// <summary>Initializes a new instance of the <see cref="Event"/> type.</summary>
         public Event()
         {
         }
 
+/// <summary>Executes this operation against the specified game.</summary>
+/// <param name="game">The game value.</param>
         public override void Execute(IGame game)
         {
             // An event should not alter the game state.
         }
 
+/// <summary>Determines whether this operation can be executed for the specified game state.</summary>
+/// <param name="gameState">The gameState value.</param>
+/// <returns>The result of the operation.</returns>
         public override bool IsExecutable(IGameState gameState)
         {
             return true;
