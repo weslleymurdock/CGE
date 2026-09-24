a2dc89b0fb4df15c05ab15224e5d9c57b5ed5bf9
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:37:04 -0300

    docs: correct constructor parameter documentation

--- src/CardGameEngine/Stat/Implementations/LifeStat.cs
+++ src/CardGameEngine/Stat/Implementations/LifeStat.cs
@@ -16,7 +16,6 @@ namespace CardGameEngine
         [JsonConstructor]
 /// <summary>Initializes a new instance of the <see cref="LifeStat"/> type.</summary>
 /// <param name="value">The value value.</param>
-/// <param name="base(value">The base(value value.</param>
 /// <param name="baseValue">The baseValue value.</param>
         public LifeStat(int value, int baseValue) : base(value, baseValue)
         {
