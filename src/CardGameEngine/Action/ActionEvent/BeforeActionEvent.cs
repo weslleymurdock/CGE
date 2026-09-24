ddb6a7345a72f35b5c160bb28f6d092769d4dac1
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:27:45 -0300

    docs: document engine methods

--- src/CardGameEngine/Action/ActionEvent/BeforeActionEvent.cs
+++ src/CardGameEngine/Action/ActionEvent/BeforeActionEvent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
 
 namespace CardGameEngine;
@@ -7,20 +7,30 @@ namespace CardGameEngine;
 public class BeforeActionEvent : ActionEvent
 {
     [JsonConstructor]
+/// <summary>Initializes a new instance of the <see cref="BeforeActionEvent"/> type.</summary>
+/// <param name="base(action">The base(action value.</param>
     public BeforeActionEvent(IAction action) : base(action)
     {
     }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
     public override object Clone()
     {
         return new BeforeActionEvent((IAction)Action.Clone());
     }
 
+/// <summary>Performs the IsAfter operation.</summary>
+/// <param name="type">The type value.</param>
+/// <returns>The result of the operation.</returns>
     public override bool IsAfter(Type type)
     {
         return false;
     }
 
+/// <summary>Performs the IsBefore operation.</summary>
+/// <param name="type">The type value.</param>
+/// <returns>The result of the operation.</returns>
     public override bool IsBefore(Type type)
     {
         return type.IsAssignableFrom(Action.GetType());
