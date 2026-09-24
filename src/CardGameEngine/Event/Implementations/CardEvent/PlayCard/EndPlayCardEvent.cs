debba0da2df39160043f622b4689517c2389444e
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:36:49 -0300

    docs: correct constructor parameter documentation

--- src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/EndPlayCardEvent.cs
+++ src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/EndPlayCardEvent.cs
@@ -6,13 +6,11 @@ namespace CardGameEngine
     public class EndPlayCardEvent : CardEvent
     {
 /// <summary>Initializes a new instance of the <see cref="EndPlayCardEvent"/> type.</summary>
-/// <param name="base(card">The base(card value.</param>
         public EndPlayCardEvent(ICard card) : base(card)
         {
         }
 
 /// <summary>Initializes a new instance of the <see cref="EndPlayCardEvent"/> type.</summary>
-/// <param name="base(getCard">The base(getCard value.</param>
         public EndPlayCardEvent(Func<ICard> getCard) : base(getCard)
         {
         }
