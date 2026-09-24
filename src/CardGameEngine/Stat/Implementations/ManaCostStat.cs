a9e8b84a440ff1a13ba3a3e794e068fdd2c746cd
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:32:29 -0300

    docs: document engine methods

--- src/CardGameEngine/Stat/Implementations/ManaCostStat.cs
+++ src/CardGameEngine/Stat/Implementations/ManaCostStat.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
 
 namespace CardGameEngine {
@@ -10,10 +10,16 @@ namespace CardGameEngine {
         /// Costs in Mana.
         /// </summary>
         [JsonConstructor]
+/// <summary>Initializes a new instance of the <see cref="ManaCostStat"/> type.</summary>
+/// <param name="value">The value value.</param>
+/// <param name="base(value">The base(value value.</param>
+/// <param name="baseValue">The baseValue value.</param>
         public ManaCostStat(int value, int baseValue) : base(value, baseValue)
         {
         }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
         public override object Clone()
         {
             return new ManaCostStat(Value, BaseValue);
