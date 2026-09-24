536e85d336b467f1b46dfaa08c940d62d55773ee
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:36:56 -0300

    docs: correct constructor parameter documentation

--- src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/PlaySpellCardEvent/StartPlaySpellCardEvent.cs
+++ src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/PlaySpellCardEvent/StartPlaySpellCardEvent.cs
@@ -6,13 +6,11 @@ namespace CardGameEngine
     public class StartPlaySpellCardEvent : StartPlayCardEvent
     {
 /// <summary>Initializes a new instance of the <see cref="StartPlaySpellCardEvent"/> type.</summary>
-/// <param name="base(spellCard">The base(spellCard value.</param>
         public StartPlaySpellCardEvent(ISpellCard spellCard) : base(spellCard)
         {
         }
 
 /// <summary>Initializes a new instance of the <see cref="StartPlaySpellCardEvent"/> type.</summary>
-/// <param name="base(getSpellCard">The base(getSpellCard value.</param>
         public StartPlaySpellCardEvent(Func<ISpellCard> getSpellCard) : base(getSpellCard)
         {
         }
