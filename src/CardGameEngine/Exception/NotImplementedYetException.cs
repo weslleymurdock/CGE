583338f248046a59e6e283a1b4714e83a4113322
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:31:55 -0300

    docs: document engine methods

--- src/CardGameEngine/Exception/NotImplementedYetException.cs
+++ src/CardGameEngine/Exception/NotImplementedYetException.cs
@@ -7,14 +7,21 @@ public class NotImplementedYetException : CardGameEngineException
 {
     private const string MessagePrefix = "This feature is not yet implemented: ";
 
+/// <summary>Initializes a new instance of the <see cref="NotImplementedYetException"/> type.</summary>
     public NotImplementedYetException()
     {
     }
 
+/// <summary>Initializes a new instance of the <see cref="NotImplementedYetException"/> type.</summary>
+/// <param name="message">The message value.</param>
     public NotImplementedYetException(string message) : base(MessagePrefix + message)
     {
     }
 
+/// <summary>Initializes a new instance of the <see cref="NotImplementedYetException"/> type.</summary>
+/// <param name="message">The message value.</param>
+/// <param name="message">The message value.</param>
+/// <param name="innerException">The innerException value.</param>
     public NotImplementedYetException(string message, Exception innerException) : base(MessagePrefix + message, innerException)
     {
     }
