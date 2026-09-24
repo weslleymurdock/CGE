792ebeef920ed57179e66f7783bc3f0483f9bb2f
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:37:13 -0300

    docs: correct constructor parameter documentation

--- src/CardGameEngine/Stat/Implementations/ManaPoolStat.cs
+++ src/CardGameEngine/Stat/Implementations/ManaPoolStat.cs
@@ -14,7 +14,6 @@ namespace CardGameEngine
         [JsonConstructor]
 /// <summary>Initializes a new instance of the <see cref="ManaPoolStat"/> type.</summary>
 /// <param name="value">The value value.</param>
-/// <param name="base(value">The base(value value.</param>
 /// <param name="baseValue">The baseValue value.</param>
         public ManaPoolStat(int value, int baseValue) : base(value, baseValue)
         {
