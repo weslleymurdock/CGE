916d9de8b3ed6817d4b5ff0289e1645392a043c1
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:30:52 -0300

    docs: document engine methods

--- src/CardGameEngine/Event/Implementations/CardEvent/CardEvent.cs
+++ src/CardGameEngine/Event/Implementations/CardEvent/CardEvent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace CardGameEngine
 {
@@ -9,10 +9,14 @@ namespace CardGameEngine
 
         protected Func<ICard> GetCard;
 
+/// <summary>Initializes a new instance of the <see cref="CardEvent"/> type.</summary>
+/// <param name="this(()">The this(() value.</param>
         public CardEvent(ICard card) : this(() => card)
         {
         }
 
+/// <summary>Initializes a new instance of the <see cref="CardEvent"/> type.</summary>
+/// <param name="getCard">The getCard value.</param>
         public CardEvent(Func<ICard> getCard)
         {
             GetCard = getCard;
