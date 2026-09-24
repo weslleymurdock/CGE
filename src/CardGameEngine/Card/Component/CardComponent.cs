7be20ad82549c4e16b825ba04dea09d0741f15c5
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:29:27 -0300

    docs: document engine methods

--- src/CardGameEngine/Card/Component/CardComponent.cs
+++ src/CardGameEngine/Card/Component/CardComponent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
@@ -12,17 +12,25 @@ public class CardComponent : Reaction, ICardComponent
 
     public List<IReaction> Reactions { get; }
 
+/// <summary>Initializes a new instance of the <see cref="CardComponent"/> type.</summary>
+/// <param name="mana">The mana value.</param>
     public CardComponent(int mana)
         : this(new ManaCostStat(mana, mana), [])
     {
     }
 
+/// <summary>Initializes a new instance of the <see cref="CardComponent"/> type.</summary>
+/// <param name="manaValue">The manaValue value.</param>
+/// <param name="manaBaseValue">The manaBaseValue value.</param>
     public CardComponent(int manaValue, int manaBaseValue)
         : this(new ManaCostStat(manaValue, manaBaseValue), [])
     {
     }
 
     [JsonConstructor]
+/// <summary>Initializes a new instance of the <see cref="CardComponent"/> type.</summary>
+/// <param name="manaCostStat">The manaCostStat value.</param>
+/// <param name="reactions">The reactions value.</param>
     protected CardComponent(ManaCostStat manaCostStat, List<IReaction> reactions)
     {
         this.manaCostStat = manaCostStat;
@@ -41,16 +49,23 @@ public class CardComponent : Reaction, ICardComponent
         set => manaCostStat.BaseValue = value;
     }
 
+/// <summary>Gets all reactions associated with this object.</summary>
+/// <returns>The result of the operation.</returns>
     public List<IReaction> AllReactions()
     {
         return [.. Reactions];
     }
 
+/// <summary>Reacts to the specified action event.</summary>
+/// <param name="game">The game value.</param>
+/// <param name="actionEvent">The actionEvent value.</param>
     public override void ReactTo(IGame game, IActionEvent actionEvent)
     {
         AllReactions().ForEach(r => r.ReactTo(game, actionEvent));
     }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
     public override object Clone()
     {
         List<IReaction> reactionsClone = [];
@@ -65,6 +80,9 @@ public class CardComponent : Reaction, ICardComponent
         );
     }
 
+/// <summary>Performs the FindCard operation.</summary>
+/// <param name="gameState">The gameState value.</param>
+/// <returns>The result of the operation.</returns>
     public ICard FindCard(IGameState gameState)
     {
         foreach (ICard card in gameState.AllCards)
