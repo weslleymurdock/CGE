70067337e3a953d0dad73ad65dc97e808485e71f
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:30:05 -0300

    docs: document engine methods

--- src/CardGameEngine/Card/SpellCard/TargetfulSpellCard/TargetfulSpellCard.cs
+++ src/CardGameEngine/Card/SpellCard/TargetfulSpellCard/TargetfulSpellCard.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
@@ -9,28 +9,47 @@ public class TargetfulSpellCard : SpellCard, ITargetfulSpellCard
 {
     public override IPlayer Owner { get; set; }
 
+/// <summary>Initializes a new instance of the <see cref="TargetfulSpellCard"/> type.</summary>
+/// <param name="owner">The owner value.</param>
+/// <param name="name">The name value.</param>
     public TargetfulSpellCard(IPlayer owner = default!, string name = "")
         : this([], owner, name)
     {
     }
 
+/// <summary>Initializes a new instance of the <see cref="TargetfulSpellCard"/> type.</summary>
+/// <param name="component">The component value.</param>
+/// <param name="owner">The owner value.</param>
+/// <param name="name">The name value.</param>
     public TargetfulSpellCard(ISpellCardComponent component, IPlayer owner, string name)
         : this([component], owner, name)
     { 
     }
 
+/// <summary>Initializes a new instance of the <see cref="TargetfulSpellCard"/> type.</summary>
+/// <param name="components">The components value.</param>
+/// <param name="owner">The owner value.</param>
+/// <param name="name">The name value.</param>
     public TargetfulSpellCard(List<ISpellCardComponent> components, IPlayer owner, string name)
         : this(components.ConvertAll(c => (ICardComponent)c), [], owner, name)
     {
     }
 
     [JsonConstructor]
+/// <summary>Initializes a new instance of the <see cref="TargetfulSpellCard"/> type.</summary>
+/// <param name="components">The components value.</param>
+/// <param name="reactions">The reactions value.</param>
+/// <param name="owner">The owner value.</param>
+/// <param name="name">The name value.</param>
     public TargetfulSpellCard(List<ICardComponent> components, List<IReaction> reactions, IPlayer owner, string name)
         : base(components, reactions, owner, name)
     {
         Owner = owner;
     }
 
+/// <summary>Gets the characters that can currently be targeted.</summary>
+/// <param name="gameState">The gameState value.</param>
+/// <returns>The result of the operation.</returns>
     public HashSet<ICharacter> GetPotentialTargets(IGameState gameState)
     {
         //Compute the intersection of all potential targets
@@ -49,6 +68,9 @@ public class TargetfulSpellCard : SpellCard, ITargetfulSpellCard
         return potentialTargets ?? [];
     }
 
+/// <summary>Casts this spell using the specified game or target.</summary>
+/// <param name="game">The game value.</param>
+/// <param name="target">The target value.</param>
     public void Cast(IGame game, ICharacter target)
     {
         if (!GetPotentialTargets(game).Contains(target))
@@ -70,6 +92,8 @@ public class TargetfulSpellCard : SpellCard, ITargetfulSpellCard
         }
     }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
     public override object Clone()
     {
         List<ICardComponent> componentsClone = [];
