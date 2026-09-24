ff1b3e116f8a4dfc3007487d903ba88273bc2878
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:28:18 -0300

    docs: document engine methods

--- src/CardGameEngine/Action/Implementations/Card/SpellCard/CastTargetfulSpellAction.cs
+++ src/CardGameEngine/Action/Implementations/Card/SpellCard/CastTargetfulSpellAction.cs
@@ -21,6 +21,8 @@ public class CastTargetfulSpellAction : CastSpellAction
         Target = target;
     }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
     public override object Clone()
     {
         return new CastTargetfulSpellAction(
@@ -31,6 +33,8 @@ public class CastTargetfulSpellAction : CastSpellAction
         );
     }
 
+/// <summary>Executes this operation against the specified game.</summary>
+/// <param name="game">The game value.</param>
     public override void Execute(IGame game)
     {
         game.Execute(new ModifyManaStatAction(Player, -SpellCard.ManaValue, 0));
@@ -39,6 +43,9 @@ public class CastTargetfulSpellAction : CastSpellAction
         game.Execute(new AddCardToGraveyardAction(Player.Graveyard, SpellCard));
     }
 
+/// <summary>Determines whether this operation can be executed for the specified game state.</summary>
+/// <param name="gameState">The gameState value.</param>
+/// <returns>The result of the operation.</returns>
     public override bool IsExecutable(IGameState gameState)
     {
         return Player == gameState.ActivePlayer
