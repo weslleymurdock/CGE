c6cbff47941b2a13af94006720012ddc9974d882
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:29:37 -0300

    docs: document engine methods

--- src/CardGameEngine/Card/Compound/Compound.cs
+++ src/CardGameEngine/Card/Compound/Compound.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
@@ -10,10 +10,14 @@ public abstract class Compound : ICompound
     public List<ICardComponent> Components { get; }
 
     [JsonConstructor]
+/// <summary>Initializes a new instance of the <see cref="Compound"/> type.</summary>
+/// <param name="components">The components value.</param>
     public Compound(List<ICardComponent> components)
     {
         Components = components;
     }
 
+/// <summary>Creates a copy of the current object.</summary>
+/// <returns>The result of the operation.</returns>
     public abstract object Clone();
 }
