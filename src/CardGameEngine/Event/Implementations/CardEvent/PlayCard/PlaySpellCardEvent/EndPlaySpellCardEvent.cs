1abadbf837dca4093914a0b6cc0b20089e5ff40b
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:31:08 -0300

    docs: document engine methods

--- src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/PlaySpellCardEvent/EndPlaySpellCardEvent.cs
+++ src/CardGameEngine/Event/Implementations/CardEvent/PlayCard/PlaySpellCardEvent/EndPlaySpellCardEvent.cs
@@ -1,13 +1,17 @@
-﻿using System;
+using System;
 namespace CardGameEngine
 {
     [Serializable]
     public class EndPlaySpellCardEvent : EndPlayCardEvent
     {
+/// <summary>Initializes a new instance of the <see cref="EndPlaySpellCardEvent"/> type.</summary>
+/// <param name="base(spellCard">The base(spellCard value.</param>
         public EndPlaySpellCardEvent(ISpellCard spellCard) : base(spellCard)
         {
         }
 
+/// <summary>Initializes a new instance of the <see cref="EndPlaySpellCardEvent"/> type.</summary>
+/// <param name="base(getSpellCard">The base(getSpellCard value.</param>
         public EndPlaySpellCardEvent(Func<ISpellCard> getSpellCard) : base(getSpellCard)
         {
         }
