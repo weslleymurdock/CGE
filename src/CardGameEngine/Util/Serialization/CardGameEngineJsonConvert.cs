756ca1671b74cc7872b22eead068d094c28c101a
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-24 01:37:20 -0300

    docs: document generic deserialization method

--- src/CardGameEngine/Util/Serialization/CardGameEngineJsonConvert.cs
+++ src/CardGameEngine/Util/Serialization/CardGameEngineJsonConvert.cs
@@ -22,6 +22,9 @@ namespace CardGameEngine
             return JsonConvert.SerializeObject(obj, serializerSettings);
         }
 
+        /// <summary>Deserializes JSON into an instance of the specified type.</summary>
+        /// <param name="json">The JSON payload to deserialize.</param>
+        /// <returns>The deserialized object.</returns>
         public static T Deserialize<T>(string json)
         {
             return JsonConvert.DeserializeObject<T>(json, serializerSettings) ?? throw new InvalidOperationException("Deserialization was not possible for this data.");
